6ee124ab090eda05fb926dca3e31355f8168c01a
huza <huzaifahasif003@gmail.com>
2025-10-01 17:31:52 +0700

    style: match main page color

--- src/rumahsakitjiwa/view/main.java
+++ src/rumahsakitjiwa/view/main.java
@@ -71,7 +71,7 @@ public class main extends JFrame {
             protected void paintComponent(Graphics g) {
                 Graphics2D g2d = (Graphics2D) g.create();
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2d.setColor(new Color(0x96A78D));
+                g2d.setColor(new Color(0x6da395));
                 g2d.fillRoundRect(0, 0, getWidth(), getHeight(), 20, 20);
                 g2d.dispose();
             }
@@ -181,7 +181,7 @@ public class main extends JFrame {
             protected void paintComponent(Graphics g) {
                 Graphics2D g2d = (Graphics2D) g.create();
                 g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-                g2d.setColor(new Color(0, 0, 0, 30));
+                g2d.setColor(new Color(0x43786e));
                 g2d.fillRect(0, 0, getWidth(), getHeight());
                 g2d.dispose();
             }
